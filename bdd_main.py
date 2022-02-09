4cf5248690970567e9e34d674d00f6880f88d245
ecriturebrailmaistupeuxrienvoir <90462132+ecriturebrailmaistupeuxrienvoir@users.noreply.github.com>
2022-02-09 08:48:24 +0100

    Add files via upload

--- bdd_main.py
+++ bdd_main.py
@@ -1,28 +1,38 @@
 import sqlite3
 from flask import *
 
-def enregistrer_score(score) :
+def enregistrer_score(score, pseudo) :
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
+    parametres = (score, pseudo)
     requete_sql = """
-    INSERT INTO bdd_scores(score)
-    VALUES (?);
+    INSERT INTO bdd_scores(score, pseudo)
+    VALUES (?, ?);
     """
-    parametre = (score,)
-    curseur.execute(requete_sql, parametre)
+    curseur.execute(requete_sql, parametres)
     connexion.commit()
     connexion.close()
 
 def lire_scores() :
     connexion = sqlite3.connect("bdd.db")
     curseur = connexion.cursor()
-    requete_sql = """
-    SELECT * FROM bdd_scores
-    """
+    requete_sql = """SELECT * FROM bdd_scores"""
     resultat = curseur.execute(requete_sql)
     scores = resultat.fetchall()
     connexion.close()
     return scores
+
+def meilleur_score() :
+    connexion = sqlite3.connect("bdd.db")
+    curseur = connexion.cursor()
+    requete_sql = """
+    SELECT MAX(score) FROM bdd_scores
+    """
+    resultat = curseur.execute(requete_sql)
+    meilleur_score = resultat.fetchall()
+    connexion.close()
+    return meilleur_score
+
 
 app = Flask(__name__)
 
@@ -33,3 +43,5 @@ def accueillir():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=1660, debug=True)
+
+print(meilleur_score())
